067a3ff4e03dcf5830757fb1f736541f73e8bab7
kzb0148 <kzb0148@auburn.edu>
2026-02-12 13:03:56 -0600

    Datavisualization_2notes

---
+++ Datavisualization_2notes.R
@@ -0,0 +1,73 @@
+#intsall and load the packages
+install.packages("ggpubr")
+installed.packages("ggrepel")
+library(tidyverse)
+library(ggpubr)
+library(ggrepel)
+
+
+cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+
+sample.data.bac <- read.csv("BacterialAlpha.csv", na.strings = "na")
+str(sample.data.bac)
+
+sample.data.bac$Time_Point <- as.factor(sample.data.bac$Time_Point)
+sample.data.bac$Crop <- as.factor(sample.data.bac$Crop)
+sample.data.bac$Crop <- factor(sample.data.bac$Crop, levels = c("Soil", "Cotton", "Soybean"))
+
+#plot one - B
+bac.even <- ggplot(sample.data.bac, aes(x = Time_Point, y = even, color = Crop))++
+  geom_boxplot(position = position_dodge(0.05)) +
+  xlab('Time') =
+  ylab("Pielou's eveness") +
+  scale_color_manual(values = cbbPalette) +
+  theme_classic()
+bac.even
+
+#Plot 2 - A
+sample.data.bac.nosoil <- subset(sample.data.bac, Crop != "Soil")
+
+water.imbibed <- ggplot(sample.data.bac.nosoil, aes(Time_Point, 1000 * Water_Imbibed, color = Crop)) +  # Define aesthetics: x-axis as Time.Point, y-axis as Water_Imbibed (converted to mg), and color by Crop
+  geom_jitter(width = 0.5, alpha = 0.5) +  # Add jittered points to show individual data points with some transparency
+  stat_summary(fun = mean, geom = "line", aes(group = Crop)) +  # Add lines representing the mean value for each Crop group
+  stat_summary(fun.data = mean_se, geom = "errorbar", width = 0.5) +  # Add error bars representing the standard error of the mean
+  xlab("Hours post sowing") +  # Label the x-axis
+  ylab("Water Imbibed (mg)") +  # Label the y-axis
+  scale_color_manual(values = c(cbbPalette[[2]], cbbPalette[[3]]), name = "", labels = c("", "")) +  # Manually set colors for the Crop variable
+  theme_classic() +  # Use a classic theme for the plot
+  theme(strip.background = element_blank(), legend.position = "none") +  # Customize theme: remove strip background and position legend to the right
+  facet_wrap(~Crop, scales = "free")  # Create separate panels for each Crop, allowing free scales
+water.imbibed
+
+#Plot -3 C
+water.imbibed.cor <- ggplot(sample.data.bac.nosoil, aes(y = even, x = 1000 * Water_Imbibed, color = Crop)) +  # Define aesthetics: y-axis as even, x-axis as Water_Imbibed (converted to mg), and color by Crop
+  geom_point(aes(shape = Time_Point)) +  # Add points with different shapes based on Time.Point
+  geom_smooth(se = FALSE, method = lm) +  # Add a linear model smooth line without confidence interval shading
+  xlab("Water Imbibed (mg)") +  # Label the x-axis
+  ylab("Pielou's evenness") +  # Label the y-axis
+  scale_color_manual(values = c(cbbPalette[[2]], cbbPalette[[3]]), name = "", labels = c("Cotton", "Soybean")) +  # Manually set colors for the Crop variable
+  scale_shape_manual(values = c(15, 16, 17, 18), name = "", labels = c("0 hrs", "6 hrs", "12 hrs", "18 hrs")) +  # Manually set shapes for the Time.Point variable
+  theme_classic() +  # Use a classic theme for the plot
+  theme(strip.background = element_blank(), legend.position = "none") +
+  facet_wrap(~Crop, scales = "free")  # Create separate panels for each Crop, allowing free scales
+
+water.imbibed.cor
+
+#Fig 2
+figure2 <- ggarrange(
+  water.imbibed,  
+  bac.even,  
+  water.imbibed.cor,  
+  labels = "auto",  
+  nrow = 3, 
+  ncol = 1,  
+  legend = FALSE
+)
+
+
+#Integrating basic stats
+bac.even + 
+  stat_compare_means(method = "anova")
+
+
